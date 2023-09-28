45151c72061134d32e8f8ba2432470c0fa3bdde6
Nzelu Ozioma <nzeluozioma555@gmail.com>
2023-09-28 01:37:26 +0100

    first commit

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <main className="About">
+      <h2>About</h2>
+      <p style={{ marginTop: "1rem" }}>
+        This blog app is fully owned by GistLovers Blog and it was built by Ozcode Global.
+      </p>
+    </main>
+  );
+};
+
+export default About;
